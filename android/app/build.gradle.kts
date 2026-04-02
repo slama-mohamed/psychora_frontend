7579e0960363ea346ad0e8d650cb0224210e7d38
Slama Mohamed <ms5878695@gmail.com>
2026-04-02 20:06:44 +0100

    pdf-view

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.psychora"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
