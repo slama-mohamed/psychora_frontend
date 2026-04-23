556a086f079238b121f5f289ff9b25cc75732cd6
Slama Mohamed <ms5878695@gmail.com>
2026-04-23 17:15:16 +0100

    duration

--- lib/core/network/network_config.dart
+++ lib/core/network/network_config.dart
@@ -4,7 +4,7 @@ class NetworkConfig {
   // Update with your backend base URL.
   static const String baseUrl = 'https://trichitic-overfrailly-sonny.ngrok-free.dev';
 
-  static const Duration connectTimeout = Duration(seconds: 20);
-  static const Duration receiveTimeout = Duration(seconds: 20);
-  static const Duration sendTimeout = Duration(seconds: 20);
+  static const Duration connectTimeout = Duration(seconds: 30);
+  static const Duration receiveTimeout = Duration(seconds: 120);
+  static const Duration sendTimeout = Duration(seconds: 30);
 }
